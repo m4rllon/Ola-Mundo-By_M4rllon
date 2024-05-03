18857cef3dbe170055d6798d8901ee5aee98a1ff
m4rllon <marllonpc33@gmail.com>
2024-05-03 10:32:56 -0300

    components.menu-link: Refatorando componente com o NavLink do react-router

--- src/components/menu-link/index.jsx
+++ src/components/menu-link/index.jsx
@@ -1,15 +1,30 @@
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, NavLink } from 'react-router-dom'
 import styles from './style.module.css'
 
 export default function MenuLink({children, to}) {
-    const localizacao = useLocation();
+    // const localizacao = useLocation();
+
+
 
     return(
-        <Link 
-        className={`
-        ${styles.link}${localizacao.pathname === to ? '' : styles.linkDestacado}`}
-        to={to}>
+        // <Link 
+        // className={`
+        // ${styles.link}${localizacao.pathname === to ? '' : styles.linkDestacado}`}
+        // to={to}>
+        //     {children}
+        // </Link>
+        
+        <NavLink 
+         to={to}
+         className={({isActive, isPending}) => 
+         [
+            isActive ? styles.linkDestacado : '',
+            isPending ? styles.link : '',
+         ].join('')}
+         end>
+            {/* A tag 'end' serve pro navegador não achar que as rotas são iguais pq todas começam com o '/' */}
             {children}
-        </Link>
+        </NavLink>
+
     )
 }
